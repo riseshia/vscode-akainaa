9b3bee978f09b6b75f260dbfb673be834719f0ac
Shia <rise.shia@gmail.com>
2024-09-11 20:43:18 +0900

    Support accumulated coverage decoration

--- src/extension.ts
+++ src/extension.ts
@@ -4,16 +4,14 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
 
-function getCoverageData(): Record<string, number[]> | null {
-    const workspaceFolders = vscode.workspace.workspaceFolders;
-    if (!workspaceFolders) {return null;}
+type DecorationTemplate = {
+    decorationType: vscode.TextEditorDecorationType,
+    decoration: { range: vscode.Range }[],
+};
 
-    const coverageFilePath = path.join(workspaceFolders[0].uri.fsPath, 'tmp/coverage.json');
-    if (!fs.existsSync(coverageFilePath)) {return null;}
-
-    const coverageData = JSON.parse(fs.readFileSync(coverageFilePath, 'utf8'));
-    return coverageData;
-}
+type ExecutedCount = number;
+type CoverageData = { lines: (ExecutedCount | null)[] };
+type CoverageResult = { [filePath: string]: CoverageData };
 
 const decorationTypes: vscode.TextEditorDecorationType[] = [1, 2, 3, 4, 5, 6, 7, 8].map(i => (
     vscode.window.createTextEditorDecorationType({
@@ -22,32 +20,102 @@ const decorationTypes: vscode.TextEditorDecorationType[] = [1, 2, 3, 4, 5, 6, 7,
     })
 ));
 
+const snapshotMaxSize = 20;
+const coverageSnapshots: CoverageResult[] = [];
+
+const lowerMaxExecuted = 5;
+const coverageFilePathFromPrj = 'tmp/coverage.json';
+
+function loadCoverageFile(): CoverageResult | null {
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (!workspaceFolders) {return null;}
+    
+    const prjDir = workspaceFolders[0].uri.fsPath + '/';
+
+    const coverageFilePath = path.join(workspaceFolders[0].uri.fsPath, coverageFilePathFromPrj);
+    if (!fs.existsSync(coverageFilePath)) {return null;}
+
+    const coverageResult: CoverageResult = JSON.parse(fs.readFileSync(coverageFilePath, 'utf8'));
+
+    const newCoverageResult: CoverageResult = {};
+    for (const filePath in coverageResult) {
+        const newFilePath = filePath.startsWith('/') ? filePath : prjDir + filePath;
+        newCoverageResult[newFilePath] = coverageResult[filePath];
+    }
+
+    return newCoverageResult;
+}
+
+function updateSnapshots() {
+    const coverageResult = loadCoverageFile();
+    if (!coverageResult) {return;}
+
+    coverageSnapshots.push(coverageResult);
+    if (coverageSnapshots.length > snapshotMaxSize) {
+        coverageSnapshots.shift();
+    }
+}
+
+function getCoverageData(): CoverageResult | null {
+    if (coverageSnapshots.length === 0) {return null;}
+
+    const result: CoverageResult = {};
+
+    for (let i = coverageSnapshots.length - 1; i >= 0; i--) {
+        const snapshot = coverageSnapshots[i];
+
+        for (const filePath in snapshot) {
+            if (!result[filePath]) {
+                result[filePath] = structuredClone(snapshot[filePath]);
+            } else {
+                const coverageData = snapshot[filePath];
+
+                // if size is different, consider file is changed and ignore this coverage data
+                if (coverageData.lines.length !== result[filePath].lines.length) {
+                    // do nothing
+                } else {
+                    const lines = snapshot[filePath].lines;
+                    const mergedLines = result[filePath].lines;
+
+                    const lineLen = lines.length;
+                    for (let i = 0; i < lineLen; i++) {
+                        if (lines[i] === null || mergedLines[i] === null) {
+                            mergedLines[i] = lines[i];
+                        } else {
+                            mergedLines[i]! += lines[i]!;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return result;
+}
+
+function updateCoverage() {
+    updateSnapshots();
+    updateDecorations();
+}
+
 function updateDecorations() {
-    console.log('updateDecorations called');
     const editor = vscode.window.activeTextEditor;
     if (!editor) {return;}
 
     const coverageData = getCoverageData();
     if (!coverageData) {return;}
 
-    const workspaceFolders = vscode.workspace.workspaceFolders;
-    if (!workspaceFolders) {return null;}
-    const prjDir = workspaceFolders[0].uri.fsPath;
-
     const filePath = editor.document.fileName;
-    const filePathFromPrjDir = filePath.substring(prjDir.length + 1);
-    const fileCoverage = coverageData[filePathFromPrjDir] || coverageData[filePath];
-    if (!fileCoverage) {return;}
+    const fileCoverageData = coverageData[filePath];
+    if (!fileCoverageData) {return;}
+    
+    const fileCoverage = fileCoverageData.lines;
 
-    let decorations: {
-        decorationType: vscode.TextEditorDecorationType,
-        decoration: { range: vscode.Range }[],
-    }[] = [0, 1, 2, 3, 4, 5, 6, 7].map(i => ({
+    let decorations: DecorationTemplate[] = [0, 1, 2, 3, 4, 5, 6, 7].map(i => ({
         decorationType: decorationTypes[i],
         decoration: [],
     }));
 
-    const maxExecuted = Math.max(...fileCoverage, 9) + 1;
+    const maxExecuted = Math.max(...fileCoverage.filter(count => count !== null), lowerMaxExecuted) + 1;
 
     for (let lineNumber = 0; lineNumber < fileCoverage.length; lineNumber++) {
         const count = fileCoverage[lineNumber];
@@ -58,9 +126,7 @@ function updateDecorations() {
 
         const line = editor.document.lineAt(lineNumber);
 
-        const decoration = {
-            range: line.range,
-        };
+        const decoration = { range: line.range };
         decorations[opacityLevel].decoration.push(decoration);
     }
 
@@ -70,11 +136,15 @@ function updateDecorations() {
 }
 
 function watchCoverageFile(context: vscode.ExtensionContext) {
-    const coverageWatcher = vscode.workspace.createFileSystemWatcher('tmp/coverage.json');
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (!workspaceFolders) {return null;}
+    const coverageFilePath = workspaceFolders[0].uri.fsPath + '/tmp/coverage.json';
+    const coverageWatcher = vscode.workspace.createFileSystemWatcher(coverageFilePath);
+    
 
-    coverageWatcher.onDidChange(() => updateDecorations());
-    coverageWatcher.onDidCreate(() => updateDecorations());
-    coverageWatcher.onDidDelete(() => updateDecorations());
+    coverageWatcher.onDidChange(() => updateCoverage());
+    coverageWatcher.onDidCreate(() => updateCoverage());
+    coverageWatcher.onDidDelete(() => updateCoverage());
 
     context.subscriptions.push(coverageWatcher);
 }
@@ -84,7 +154,7 @@ function watchCoverageFile(context: vscode.ExtensionContext) {
 export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "akainaa" is now active!');
+	console.log('akainaa is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -100,6 +170,7 @@ export function activate(context: vscode.ExtensionContext) {
     vscode.window.onDidChangeActiveTextEditor(updateDecorations, null, context.subscriptions);
     vscode.workspace.onDidChangeTextDocument(updateDecorations, null, context.subscriptions);
 
+    updateSnapshots();
     if (vscode.window.activeTextEditor) {
         updateDecorations();
     }
